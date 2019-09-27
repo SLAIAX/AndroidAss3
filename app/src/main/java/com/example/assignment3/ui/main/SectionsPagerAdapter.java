425ac76b4da1d1b454d3607d8b75b04aa259fce2
SLAIAX <1burgerboys@gmail.com>
2019-09-27 14:09:36 +1200

    Layouts basic setup and file creation

--- app/src/main/java/com/example/assignment3/ui/main/SectionsPagerAdapter.java
+++ app/src/main/java/com/example/assignment3/ui/main/SectionsPagerAdapter.java
@@ -17,7 +17,7 @@ import com.example.assignment3.R;
 public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @StringRes
-    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2};
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2,R.string.tab_text_3};
     private final Context mContext;
 
     public SectionsPagerAdapter(Context context, FragmentManager fm) {
@@ -29,7 +29,17 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
     public Fragment getItem(int position) {
         // getItem is called to instantiate the fragment for the given page.
         // Return a PlaceholderFragment (defined as a static inner class below).
-        return PlaceholderFragment.newInstance(position + 1);
+        switch (position){
+            case 0:
+                return ProfileFragment.newInstance(position + 1);
+            case 1:
+                return HomeFragment.newInstance(position + 1);
+            case 2:
+                return ChallengehubFragment.newInstance(position + 1);
+            default:
+                return HomeFragment.newInstance(position + 1);
+        }
+
     }
 
     @Nullable
@@ -40,7 +50,7 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public int getCount() {
-        // Show 2 total pages.
-        return 2;
+        // Show 3 total pages.
+        return 3;
     }
 }
