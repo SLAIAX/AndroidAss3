d7b970db746fc6f19cfd61edb6865d30043221fc
SLAIAX <1burgerboys@gmail.com>
2019-10-01 11:49:39 +1300

    Minor testing modifications

--- app/src/main/java/com/example/assignment3/NameActivity.java
+++ app/src/main/java/com/example/assignment3/NameActivity.java
@@ -3,6 +3,9 @@ package com.example.assignment3;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -14,11 +17,21 @@ public class NameActivity extends AppCompatActivity {
     public EditText name;
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_name);
 
         name = findViewById(R.id.nameEntered);
-
+//        name.setOnKeyListener((view, i, keyEvent) -> {
+//            System.out.println("OPENED KEY");
+//            if (i==KeyEvent.KEYCODE_ENTER) {
+//                System.out.println("OPENED KEY ENTER");
+//                openApp();
+//                return true;
+//            }
+//            return false;
+//        });
 
         Button continueButton = findViewById(R.id.continueButton);
         continueButton.setOnClickListener(v -> openApp());
@@ -26,6 +39,7 @@ public class NameActivity extends AppCompatActivity {
 
     public void openApp(){
         String nameTemp = name.getText().toString();
+        System.out.println("OPENED");
         if(nameTemp.length() == 0){
             Toast toast = Toast.makeText(this,"Please enter a name", Toast.LENGTH_LONG);
             toast.show();
